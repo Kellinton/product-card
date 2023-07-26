7ab38e3b878d3b29dd37114a8a0189710b1d8ea6
Kellinton <kellintonbrasil@gmail.com>
2023-07-26 16:49:20 -0300

    adicionando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors:{
+      'primary': 'hsl(158, 36%, 37%)',
+      'primary1': 'hsl(30, 38%, 92%)',
+      'neutral': 'hsl(212, 21%, 14%)',
+      'neutral1': 'hsl(228, 12%, 48%)',
+      'neutral2': 'hsl(0, 0%, 100%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
