99de7c9e1ae9bb078622bd255f25f501495080f5
Kellinton <kellintonbrasil@gmail.com>
2023-07-28 14:37:38 -0300

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
   content: [],
   theme: {
-    colors:{
-      'primary': 'hsl(158, 36%, 37%)',
-      'primary1': 'hsl(30, 38%, 92%)',
-      'neutral': 'hsl(212, 21%, 14%)',
-      'neutral1': 'hsl(228, 12%, 48%)',
-      'neutral2': 'hsl(0, 0%, 100%)'
-    },
     extend: {},
   },
   plugins: [],
